108cea9e4e2413b4170757100e41f8852fefcef2
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2020-07-25 11:38:14 -0300

    Fixed Routes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')// requisitando o express
 const nunjucks = require('nunjucks')
+const routes = require('./routes')
 
 const server = express()//inserindo o express no server
-const videos = require('./data')//está referenciando o arquivo na raiz
 
 server.use(express.static('public'))
+server.use(routes)
 
 //configurando template engine
 server.set('view engine', 'njk')
@@ -15,41 +16,7 @@ nunjucks.configure('views', {
     noCache: true
 })
 
-//levar informações do backend para o front, pagina Sobre
-server.get('/', function (req,res) {
-    const about = {
-        avatar_url:"https://avatars0.githubusercontent.com/u/61247355?s=460&u=4",
-        name:"Gabriel Cordeiro",
-        role:"Desenvolvedor",
-        description:"Programador Full-Stack",
-        links:[
-            { name:"Github", url:"https://github.com/gabriel-cordeiro-dev" },
-            { name:"Instagram", url:"https://www.instagram.com/gabrielluuckner/" },
-            { name:"Linkedin", url:"https://www.linkedin.com/in/gabriel-cordeiro-033641144/" }
-        ]
-    }
-    return res.render('about', {about: about})
-})
 
-server.get('/certificacoes', function (req,res) {
-
-    return res.render('certificacoes', {items: videos})
-})
-
-//configurando requisição de video em outro endereço
-server.get('/video', function (req,res) {
-    const id = req.query.id
-
-    const video = videos.find(function(video){
-        return video.id == id
-    })
-
-    if (!video) {
-        return res.send("Video not found!")
-    }
-
-    return res.render('video', { item: video })
-})
 
 //configurar servidor na porta 5000
 server.listen(5000, function () {
