098ea9e3f90ded475810ce122e19418184da5677
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2020-08-03 21:23:55 -0300

    validação de dados
    validando e organizando dados instrutores, assim como novas rotas

---
+++ instructors.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const data = require('./data.json')
+
+//show
+exports.show = function (req, res){
+    //req.params
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function (instructor){
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found')
+
+    return res.send(foundInstructor)
+}
+//create
+exports.post = function (req, res){
+    
+    //realizando uma estrutura de validação
+    const keys = Object.keys(req.body)//o keys criou um array com as chaves dos objetos
+
+    for(key of keys){
+       if (req.body[key] == ""){
+          return res.send('Please, fill all fields') 
+       }
+    }
+
+    let {avatar_url,birth,services,gender,name} = req.body //desestruturando o req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)// cria uma ID para cada nova entrada de dados
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+    }) // aqui ele retorna um array com um objeto dentro [{...}]
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send('Write File error')
+
+        return res.redirect('/instructors')
+    })
+
+    //return res.send(req.body)
+}
+//update
+
+//delete
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', function (req, res){
     return res.redirect('/instructors')
@@ -13,20 +14,10 @@ routes.get('/instructors/create', function (req, res){
     return res.render('instructors/create')
 })
 
+routes.get('/instructors/:id', instructors.show)
+
 //usando o metodo post, tem que usar o req.body, se fosse metedo get, usaria req.query
-routes.post('/instructors', function (req, res){
-    
-    //realizando uma estrutura de validação
-    const keys = Object.keys(req.body)//o keys criou um array com as chaves dos objetos
-
-    for(key of keys){
-       if (req.body[key] == ""){
-          return res.send('Please, fill all fields') 
-       }
-    }
-
-    return res.send(req.body)
-})
+routes.post('/instructors', instructors.post)
 
 routes.get('/members', function (req, res){
     return res.send('members')
