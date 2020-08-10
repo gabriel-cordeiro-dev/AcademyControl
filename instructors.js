92411a5533bfa27ca567db43bba87608d0b8d5ab
Gabriel Cordeiro <gabriel.cord18@gmail.com>
2020-08-10 17:54:47 -0300

    .

--- instructors.js
+++ instructors.js
@@ -2,6 +2,12 @@ const fs = require('fs');
 const data = require('./data.json')
 const { age, date } = require('./utils')
 
+exports.index = function (req, res){
+
+    return res.render('instructors/index', {instructors: data.instructors})
+}
+
+
 //show
 exports.show = function (req, res){
     //req.params
@@ -96,7 +102,8 @@ exports.put = function (req, res){
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[index] = instructor
